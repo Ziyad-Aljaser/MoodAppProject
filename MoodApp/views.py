9e2d18c198feebadb61243ca779c1f31943cfcfb
Ziyad-Aljaser <Ziyad.M.Aljaser@gmail.com>
2022-12-13 09:53:46 -0500

    API Key Updated

--- MoodApp/views.py
+++ MoodApp/views.py
@@ -5,10 +5,10 @@ from .models import *
 import os
 import openai
 
-# Used to generate the random AI image baed on the mood
+# Used to generate the random AI image based on the user mood
 def get_AI_image(mode):
-    secret = "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"
-    openai.api_key = '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'  # api key
+    secret = "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"
+    openai.api_key = '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'  # api key
     test = openai.Image.create(prompt=mode, n=1, size="1024x1024")
     url = str(test["data"][0])[12:-3]
     return url
